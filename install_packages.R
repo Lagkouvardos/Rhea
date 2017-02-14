e77385bba3d84b6774c1a9dc9326bf881eef72f5
sandraFischer <sandra.fischer@tum.de>
2017-02-14 15:38:52 +0100

    install packages

---
+++ install_packages.R
@@ -0,0 +1,45 @@
+#' This script installs all required libraries automatically
+#' Please install libraries manually if it was not possible to install an library automatically
+#' Missing libraries are listed in missing_packages.txt
+#' To install an library please use following two command:
+#' install.packages("name of the missing library")
+#' library("name of the missing library)
+
+##################################################################################
+######                             Main Script                              ###### 
+##################################################################################
+
+
+###################       Load all required libraries     ########################
+
+# Check if required packages are already installed, and install if missing
+
+packages <-c("ade4","GUniFrac","phangorn","cluster",
+             "fpc","compare","plotrix","PerformanceAnalytics","reshape","ggplot2","gridExtra","grid","ggrepel",
+             "gtable","Matrix","cowplot", "Hmisc","corrplot","sandra","panda")
+
+# Function to check whether the package is installed
+InsPack <- function(pack)
+{
+  if ((pack %in% installed.packages()) == FALSE) {
+    install.packages(pack,repos = "http://cloud.r-project.org/")
+  } 
+}
+
+
+
+# Applying the installation on the list of packages
+lapply(packages, InsPack)
+
+# Make the libraries
+lib <- lapply(packages, require, character.only = TRUE)
+not_installed <- which(lib==FALSE)
+missing_packages <- lapply(not_installed, function(x) print(packages[x]))
+
+
+# Adding log file in analysis
+sink(file = "missing_packages.txt")
+cat ("***************************************","\n")
+cat ("Please install following packages manually","\n")
+cat (as.character(missing_packages))
+sink()
