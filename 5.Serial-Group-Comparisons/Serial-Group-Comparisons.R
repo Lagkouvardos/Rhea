Lagkouvardos/Rhea

commit 903e22d5a17cabd548b2d8ad19286f56741beff9

Column names
Column names fo the Input_Table are saved to avoid losing information while removing columns

5.Serial-Group-Comparisons/Serial-Group-Comparisons.R
@@ -209,8 +209,14 @@ my_otu_mod =  as.data.frame(apply(my_otu_data,2,abundance.fix))
 # Column consisting of "NA" or "0" only are removed (see below)
 my_otu_mod_noz = as.data.frame(apply(my_otu_mod,2,fill_zero.NA,ReplaceZero))
 
+# Save column names for columns without zeros
+namesHeader <- apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0)))
+
 # Remove column if entire OTU column contain zeros or missing values
-my_otu_mod_noz <- my_otu_mod_noz[,!apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0)))]
+my_otu_mod_noz <- as.data.frame(my_otu_mod_noz[,!apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0)))])
+
+# Add column names to the dataset
+colnames(my_otu_mod_noz) <- names(namesHeader[-which(namesHeader==TRUE)])
 
 # Transform data by removing any OTU with median relative abundance below cutoff
 t_otu_mod_noz = as.data.frame(t(my_otu_mod_noz))

commit 21f5efc8bdaf6aaee760c942ba124d2c331f7370

Keep the name of the organisms
This bug was introduced in 903e22d5a17cabd548b2d8ad19286f56741beff9

5.Serial-Group-Comparisons/Serial-Group-Comparisons.R
@@ -209,15 +209,9 @@ my_otu_mod =  as.data.frame(apply(my_otu_data,2,abundance.fix))
 # Column consisting of "NA" or "0" only are removed (see below)
 my_otu_mod_noz = as.data.frame(apply(my_otu_mod,2,fill_zero.NA,ReplaceZero))
 
-# Save column names for columns without zeros
-namesHeader <- apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0)))
-
 # Remove column if entire OTU column contain zeros or missing values
-my_otu_mod_noz <- as.data.frame(my_otu_mod_noz[,!apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0)))])
-
-# Add column names to the dataset
-colnames(my_otu_mod_noz) <- names(namesHeader[-which(namesHeader==TRUE)])
-
+my_otu_mod_noz <- my_otu_mod_noz[,!apply(my_otu_mod_noz , 2 , function(x) all(is.na(x) | (x == 0))), drop = FALSE]
+                                                       
 # Transform data by removing any OTU with median relative abundance below cutoff
 t_otu_mod_noz = as.data.frame(t(my_otu_mod_noz))
 