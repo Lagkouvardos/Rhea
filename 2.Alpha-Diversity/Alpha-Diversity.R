14c787ec9f311530c36bc161f50e99b31b7c7798
Ilias Lagkouvardos <Lagkouvardos@users.noreply.github.com>
2020-01-14 09:43:26 +0200

    Update Alpha-Diversity.R
    Added Normalized and Effective Richness

--- 2.Alpha-Diversity/Alpha-Diversity.R
+++ 2.Alpha-Diversity/Alpha-Diversity.R
@@ -1,10 +1,21 @@
-#' Version 1.3
-#' Last modified on 22/02/2016
+#' Version 2.0
+#' Last modified on 20/01/2020
 #' Script Task: Calculate alpha-diversity
 #' Author: Ilias Lagkouvardos
+#' Contributions by: Thomas Clavel 
+
 #'
-#' For meaningful species richness, use of normalized sequence counts is expected.
-#' For richness calculation, only OTUs that are above 0.5 normalized counts are considered.
+#' For meaningful comparisons of species richness across samples, 
+#' use of normalized sequence counts is expected.
+#' For normalized richness calculation, only OTUs that are above 0.5 normalized counts are considered.
+#' For comparisons across studies the notions of "Normalized" and "Effective" richness are being used.
+#' Effective Richness is the number of OTUs occurring at a relative abundance of 0.25% in a given sample, a threshold that we experimentally determined using Mock communities in vitro and in vivo and is efficient in removing spurious taxa from amplicon sequencing data.
+#' Normalized Richness is the number of OTUs above 0.5 counts in a sample when normalized to a fixed depth (1000)
+#' 
+#' Note:
+#' Files are stored in the current folder 
+#' If a file is needed for downstream analysis, it is also automatically added to the appropriate next folder
+#' under the condition that the original folder structure of Rhea is maintained.
 
 ##################################################################################
 ######             Set parameters in this section manually                  ######
@@ -12,10 +23,17 @@
 
 #' Please set the directory of the script as working folder (e.g D:/MyStudy/NGS/Rhea/alpha-diversity)
 #' Note: the path is denoted by forward slash "/"
-setwd("D:/path/to/Rhea/2.Alpha-Diversity")    #<--- CHANGE ACCORDINGLY
+setwd("C:/path/to/Rhea/2.Alpha-Diversity")    #<--- CHANGE ACCORDINGLY
 
 #' Please give the file name of the normalized OTU-table (without taxonomic classification)
 file_name <- "OTUs_Table-norm.tab"                #<--- CHANGE ACCORDINGLY
+
+#' The abundance filtering cutoff 
+eff.cutoff <- 0.0025 # this is the default value for Effective Richness (0.25%)
+
+#' The normalized depth cutoff
+norm.cutoff <- 1000 # this is the default value for Standard Richness (1000)
+
 
 ######                  NO CHANGES ARE NEEDED BELOW THIS LINE               ######
 
@@ -30,6 +48,26 @@ Species.richness <- function(x)
   count=sum(x[x>0.5]^0)
   return(count)
 }
+
+# Calculate the Effective species richness in each individual sample
+Eff.Species.richness <- function(x)
+{
+  # Count only the OTUs that are present more than the set proportion
+  total=sum(x)
+  count=sum(x[x/total>eff.cutoff]^0)
+  return(count)
+}
+
+# Calculate the Normalized species richness in each individual sample
+Norm.Species.richness <- function(x)
+{
+  # Count only the OTUs that are present >0.5 normalized counts (normalization produces real values for counts)
+  # Given a fixed Normalization reads depth
+  total=sum(x)
+  count=sum(x[norm.cutoff*x/total>0.5]^0)
+  return(count)
+}
+
 
 # Calculate the Shannon diversity index
 Shannon.entropy <- function(x)
@@ -85,10 +123,12 @@ my_otu_table <-data.frame(t(my_otu_table))
 # Apply diversity functions to table
 otus_div_stats<-data.frame(my_otu_table[,0])
 otus_div_stats$Richness<-apply(my_otu_table,1,Species.richness)
-otus_div_stats$Shannon<-apply(my_otu_table,1,Shannon.entropy)
-otus_div_stats$Shannon.effective<-apply(my_otu_table,1,Shannon.effective)
-otus_div_stats$Simpson<-apply(my_otu_table,1,Simpson.concentration)
-otus_div_stats$Simpson.effective<-apply(my_otu_table,1,Simpson.effective)
+otus_div_stats$Normalized.Richness<-apply(my_otu_table,1,Norm.Species.richness)
+otus_div_stats$Effective.Richness<-apply(my_otu_table,1,Eff.Species.richness)
+otus_div_stats$Shannon.Index<-apply(my_otu_table,1,Shannon.entropy)
+otus_div_stats$Shannon.Effective<-apply(my_otu_table,1,Shannon.effective)
+otus_div_stats$Simpson.Index<-apply(my_otu_table,1,Simpson.concentration)
+otus_div_stats$Simpson.Effective<-apply(my_otu_table,1,Simpson.effective)
 otus_div_stats$Evenness <- otus_div_stats$Shannon/log(otus_div_stats$Richness,2)
 
 
