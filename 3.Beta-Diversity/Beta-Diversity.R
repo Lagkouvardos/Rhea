8d4362373ac8549fe625a1da7826139d603224c3
Ilias Lagkouvardos <Lagkouvardos@users.noreply.github.com>
2021-01-18 17:13:12 +0200

    Update Beta-Diversity.R
    beta diversity now looks on the mapping file to collect the identifiers of the targeted samples.
    Mapping file can be a subsection of the whole OTU table and the script will extract only those referred to the mapping file .

--- 3.Beta-Diversity/Beta-Diversity.R
+++ 3.Beta-Diversity/Beta-Diversity.R
@@ -1,5 +1,8 @@
 #' Script: Beta-Diversity
+#' Version: 1.1
+#' Last modified on 08/12/2020
 #' Author: Ilias Lagkouvardos
+#' Contributions by: Thomas Clavel, Sandra Reitmeier
 #'
 #' Calculate beta-diversity for microbial communities
 #' based on permutational mulitvariate analysis of variances (PERMANOVA) using multiple distance matrices
@@ -98,33 +101,11 @@ flag <- all(as.logical(lib))
 
 
 ###################       Read all required input files      ####################
-
-# Load the tab-delimited file containing the values to be analyzed (samples names in the first column)
-otu_file <- read.table (file = input_otu, check.names = FALSE, header = TRUE, dec = ".", sep = "\t", row.names = 1, comment.char = "")
-
-# Clean table from empty lines
-otu_file <- otu_file[!apply(is.na(otu_file) | otu_file =="",1,all),]
-
 # Load the mapping file containing individual sample information (sample names in the first column)
 meta_file <- read.table (file = input_meta, check.names = FALSE, header = TRUE, dec = ".", sep = "\t", row.names = 1, comment.char = "")
 
 # Clean table from empty lines
 meta_file <- data.frame(meta_file[!apply(is.na(meta_file) | meta_file=="",1,all),])
-
-# Load the phylogenetic tree calculated from the OTU sequences 
-tree_file <- read.tree(input_tree)
-
-# Create the directory where all output files are saved (is named after the target group name set above for comparisons)
-dir.create(group_name)
-
-####################       Calculate beta-diversity          ###################
-
-# OTU-table and mapping file should have the same order and number of sample names
-# Order the OTU-table by sample names (ascending)
-otu_file <- otu_file[,order(names(otu_file))]
-
-# Transpose OTU-table and convert format to a data frame
-otu_file <- data.frame(t(otu_file))
 
 # Order the mapping file by sample names (ascending)
 meta_file <- data.frame(meta_file[order(row.names(meta_file)),])
@@ -135,8 +116,37 @@ meta_file_pos <- which(colnames(meta_file) == group_name)
 # Select metadata group based on the pre-set group name
 all_groups <- as.factor(meta_file[,meta_file_pos])
 
+#------------------------------------------------------------------------
+
+# Load the tab-delimited file containing the values to be analyzed (samples names in the first column)
+otu_file <- read.table (file = input_otu, check.names = FALSE, header = TRUE, dec = ".", sep = "\t", row.names = 1, comment.char = "")
+
+# Clean table from empty lines
+otu_file <- otu_file[!apply(is.na(otu_file) | otu_file =="",1,all),]
+
+# keep only those rows that appear in the mapping file
+otu_file <- otu_file[,rownames(meta_file)]
+
+# OTU-table and mapping file should have the same order and number of sample names
+# Order the OTU-table by sample names (ascending)
+otu_file <- otu_file[,order(names(otu_file))]
+
+# Transpose OTU-table and convert format to a data frame
+otu_file <- data.frame(t(otu_file))
+
+#------------------------------------------------------------------------
+
+# Load the phylogenetic tree calculated from the OTU sequences 
+tree_file <- read.tree(input_tree)
+
 # Root the OTU tree at midpoint 
 rooted_tree <- midpoint(tree_file)
+
+
+####################       Calculate beta-diversity          ###################
+
+# Create the directory where all output files are saved (is named after the target group name set above for comparisons)
+dir.create(group_name)
 
 # Calculate the UniFrac distance matrix for comparing microbial communities
 unifracs <- GUniFrac(otu_file, rooted_tree, alpha = c(0.0,0.5,1.0))$unifracs
@@ -213,100 +223,100 @@ dev.off()
 # Get all groups contained in the mapping file
 unique_groups <- levels(all_groups_comp)
 if (dim(table(unique_groups)) > 2) {
-
-# Initialise vector and lists
-pVal = NULL
-pairedMatrixList <- list(NULL)
-pair_1_list <- NULL
-pair_2_list <- NULL
-
-for (i in 1:length(combn(unique_groups,2)[1,])) {
- 
-  # Combine all possible pairs of groups
-  pair_1 <- combn(unique_groups,2)[1,i]
-  pair_2 <- combn(unique_groups,2)[2,i]
   
-  # Save pairs information in a vector
-  pair_1_list[i] <- pair_1
-  pair_2_list[i] <- pair_2
+  # Initialise vector and lists
+  pVal = NULL
+  pairedMatrixList <- list(NULL)
+  pair_1_list <- NULL
+  pair_2_list <- NULL
+  
+  for (i in 1:length(combn(unique_groups,2)[1,])) {
     
-  # Generate a subset of all samples within the mapping file related to one of the two groups
-  inc_groups <-
-    rownames(subset(meta_file, meta_file[,meta_file_pos] == pair_1
-                    |
-                      meta_file[,meta_file_pos] == pair_2))
+    # Combine all possible pairs of groups
+    pair_1 <- combn(unique_groups,2)[1,i]
+    pair_2 <- combn(unique_groups,2)[2,i]
+    
+    # Save pairs information in a vector
+    pair_1_list[i] <- pair_1
+    pair_2_list[i] <- pair_2
+    
+    # Generate a subset of all samples within the mapping file related to one of the two groups
+    inc_groups <-
+      rownames(subset(meta_file, meta_file[,meta_file_pos] == pair_1
+                      |
+                        meta_file[,meta_file_pos] == pair_2))
+    
+    # Convert UniFrac distance matrix to data frame
+    paired_dist <- as.data.frame(unifract_dist_comp)
+    
+    # Save all row names of the mapping file
+    row_names <- rownames(paired_dist)
+    
+    # Add row names to the distance matrix
+    paired_dist <- cbind(row_names,paired_dist)
+    
+    # Generate distance matrix with samples of the compared groups (column-wise)
+    paired_dist <- paired_dist[sapply(paired_dist[,1], function(x) all(x %in% inc_groups)),]
+    
+    # Remove first column with unnecessary group information
+    paired_dist[,1] <- NULL
+    paired_dist <- rbind(row_names,paired_dist)
+    
+    # Generate distance matrix with samples of the compared group (row-wise)
+    paired_dist <- paired_dist[,sapply(paired_dist[1,], function(x) all(x %in% inc_groups))]
+    
+    # Remove first row with unnecessary group information 
+    paired_dist <- paired_dist[-1,]
+    
+    # Convert generated distance matrix to data type matrix (needed by multivariate analysis)
+    paired_matrix <- as.matrix(paired_dist)
+    class(paired_matrix) <- "numeric"
+    
+    # Save paired matrix in list
+    pairedMatrixList[[i]] <- paired_matrix
+    
+    # Applies multivariate analysis to a pair out of the selected groups
+    adonis <- adonis(paired_matrix ~ all_groups_comp[all_groups_comp == pair_1 |
+                                                       all_groups_comp == pair_2])
+    
+    # List p-values
+    pVal[i] <- adonis[[1]][6][[1]][1]
+    
+  }
   
-  # Convert UniFrac distance matrix to data frame
-  paired_dist <- as.data.frame(unifract_dist_comp)
+  # Adjust p-values for multiple testing according to Benjamini-Hochberg method
+  pVal_BH <- p.adjust(pVal,method="BH", n=length(pVal))
   
-  # Save all row names of the mapping file
-  row_names <- rownames(paired_dist)
+  # Generated NMDS plots are stored in one pdf file called "pairwise-beta-diversity-nMDS.pdf"
+  file_name <- paste(group_name,"pairwise-beta-diversity-NMDS.pdf",sep="_")
+  pdf(paste(group_name,"/",file_name,sep=""))
   
-  # Add row names to the distance matrix
-  paired_dist <- cbind(row_names,paired_dist)
-  
-  # Generate distance matrix with samples of the compared groups (column-wise)
-  paired_dist <- paired_dist[sapply(paired_dist[,1], function(x) all(x %in% inc_groups)),]
-  
-  # Remove first column with unnecessary group information
-  paired_dist[,1] <- NULL
-  paired_dist <- rbind(row_names,paired_dist)
-  
-  # Generate distance matrix with samples of the compared group (row-wise)
-  paired_dist <- paired_dist[,sapply(paired_dist[1,], function(x) all(x %in% inc_groups))]
-  
-  # Remove first row with unnecessary group information 
-  paired_dist <- paired_dist[-1,]
-  
-  # Convert generated distance matrix to data type matrix (needed by multivariate analysis)
-  paired_matrix <- as.matrix(paired_dist)
-  class(paired_matrix) <- "numeric"
-  
-  # Save paired matrix in list
-  pairedMatrixList[[i]] <- paired_matrix
-  
-  # Applies multivariate analysis to a pair out of the selected groups
-  adonis <- adonis(paired_matrix ~ all_groups_comp[all_groups_comp == pair_1 |
-                                            all_groups_comp == pair_2])
-  
-  # List p-values
-  pVal[i] <- adonis[[1]][6][[1]][1]
-  
-}
-
-# Adjust p-values for multiple testing according to Benjamini-Hochberg method
-pVal_BH <- p.adjust(pVal,method="BH", n=length(pVal))
-
-# Generated NMDS plots are stored in one pdf file called "pairwise-beta-diversity-nMDS.pdf"
-file_name <- paste(group_name,"pairwise-beta-diversity-NMDS.pdf",sep="_")
-pdf(paste(group_name,"/",file_name,sep=""))
-
-for(i in 1:length(combn(unique_groups,2)[1,])){
+  for(i in 1:length(combn(unique_groups,2)[1,])){
     meta <- metaMDS(pairedMatrixList[[i]], k = 2)
     s.class(
       meta$points,
       col = rainbow(length(levels(all_groups_comp))), cpoint = 2,
       fac = as.factor(all_groups_comp[all_groups_comp == pair_1_list[i] |
-                                   all_groups_comp == pair_2_list[i]]),
+                                        all_groups_comp == pair_2_list[i]]),
       sub = paste("NMDS plot of Microbial Profiles\n ",pair_1_list[i]," - ",pair_2_list[i], "\n(p-value ",pVal[i],","," corr. p-value ", pVal_BH[i],")",sep="")
     )
-}
-dev.off()
-
-# Generated MDS plots are stored in one pdf file called "pairwise-beta-diversity-MDS.pdf"
-file_name <- paste(group_name,"pairwise-beta-diversity-MDS.pdf",sep="_")
-pdf(paste(group_name,"/",file_name,sep=""))
-
-for(i in 1:length(combn(unique_groups,2)[1,])){
-  # Calculate and display the MDS plot (Multidimensional Scaling plot)
-  s.class(
-    cmdscale(pairedMatrixList[[i]], k = 2), col = rainbow(length(levels(all_groups_comp))), cpoint =
-      2, fac = as.factor(all_groups_comp[all_groups_comp == pair_1_list[i] |
-                                           all_groups_comp == pair_2_list[i]]), sub = paste("MDS plot of Microbial Profiles\n ",pair_1_list[i]," - ",pair_2_list[i], "\n(p-value ",pVal[i],","," corr. p-value ", pVal_BH[i],")",sep="")
-  )
-}
-dev.off()                                     
-                                     
+  }
+  dev.off()
+  
+  # Generated MDS plots are stored in one pdf file called "pairwise-beta-diversity-MDS.pdf"
+  file_name <- paste(group_name,"pairwise-beta-diversity-MDS.pdf",sep="_")
+  pdf(paste(group_name,"/",file_name,sep=""))
+  
+  for(i in 1:length(combn(unique_groups,2)[1,])){
+    # Calculate and display the MDS plot (Multidimensional Scaling plot)
+    s.class(
+      cmdscale(pairedMatrixList[[i]], k = 2), col = rainbow(length(levels(all_groups_comp))), cpoint =
+        2, fac = as.factor(all_groups_comp[all_groups_comp == pair_1_list[i] |
+                                             all_groups_comp == pair_2_list[i]]), sub = paste("MDS plot of Microbial Profiles\n ",pair_1_list[i]," - ",pair_2_list[i], "\n(p-value ",pVal[i],","," corr. p-value ", pVal_BH[i],")",sep="")
+    )
+  }
+  dev.off()                                     
+  
 }
 
 ######                        Determine number of clusters                           ######
@@ -353,7 +363,7 @@ write.tree(tree,"samples-Tree.nwk",tree.names = FALSE)
 
 # Graphical output files are generated in the main part of the script
 if(!flag) { stop("
-    It was not possible to install all required R libraries properly.
+                 It was not possible to install all required R libraries properly.
                  Please check the installation of all required libraries manually.\n
                  Required libaries:ade4, GUniFrac, phangorn")
 }
